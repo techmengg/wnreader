dab236239c5be1793d32d10911327b0895a73b4d
techmengg <aingtechmeng@gmail.com>
2025-11-17 05:16:37 -0500

    fix: infinite redirect coming from middleware

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ const publicRoutes = ["/login", "/register", "/api/register"];
 
 export default authEdge((request) => {
   const { nextUrl } = request;
-  const isAuthenticated = Boolean(request.auth);
+  const isAuthenticated = Boolean(request.auth?.user);
   const pathname = nextUrl.pathname;
 
   const isPublic = publicRoutes.some((route) => pathname.startsWith(route));
